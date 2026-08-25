ab72a20a845a6d247d11fa6db2f71fc1eaf26d9a
MaheshaC2 <256048071+MaheshaC2@users.noreply.github.com>
2026-08-25 22:34:09 +0530

    Fix duplicate print statement for non-anagrams

--- Anagram.py
+++ Anagram.py
@@ -2,7 +2,6 @@
 
 word1 = input("Enter first word: ").replace(" ", "").lower()
 word2 = input("Enter second word: ").replace(" ", "").lower()
-
 if sorted(word1) == sorted(word2):
     print("They are anagrams.")
 else:
